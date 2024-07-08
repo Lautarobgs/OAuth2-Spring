f1cd0b584de68fef40341e8ef2a96aff3e6c3486
Lautarobgs <lautarobgs2k@gmail.com>
2024-07-08 01:47:04 -0300

    Prueba oauth2 con github!

---
+++ src/main/java/com/example/ejemploOauth/EjemploOauthApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejemploOauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploOauthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ejemploOauth/EjemploOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejemploOauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
